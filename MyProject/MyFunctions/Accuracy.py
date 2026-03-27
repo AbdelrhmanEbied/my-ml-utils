754ba601f96a5b9f1916e865a62a8a330cef2a13
AbdelrhmanEbied <abdelrhmanebied170@gmail.com>
2026-03-27 22:49:30 +0200

    Delete MyProject/MyFunctions/Accuracy.py

--- MyProject/MyFunctions/Accuracy.py
+++
@@ -1,5 +0,0 @@
-import torch 
-def accuracy_fn(y_pred, y_true):
-    predicted_classes = torch.argmax(y_pred, dim=1)
-    correct = (predicted_classes == y_true).sum().item()
-    return correct / len(y_true)
